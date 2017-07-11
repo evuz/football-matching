04c0cd24d35a9d51d950fc8c8e9b021ace4ab80f
evuz <jesus_jgb_45@hotmail.com>
2017-07-11 23:01:53 +0200

    App: use LoginContainer

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '@/containers/Header';
+import Login from '@/containers/LoginRoute';
 
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Header></Header>
+    <Login></Login>
   </div>
 );
 
