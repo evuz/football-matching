d3d10a2a1614df62d59eb93602f33665677162ae
evuz <jesus_jgb_45@hotmail.com>
2017-07-11 14:03:38 +0200

    App: Integrate redux and react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import configureStore from './configureStore';
 
 import App from './app/App';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { store, history } = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App history={history} />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
