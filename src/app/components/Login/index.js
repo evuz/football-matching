2fcc479dfeeb896d6340efddeda5d74a15ca07c1
evuz <jesus_jgb_45@hotmail.com>
2017-07-12 14:17:50 +0200

    Login Component: Introduce input with children props

--- src/app/components/Login/index.js
+++ src/app/components/Login/index.js
@@ -1,47 +1,14 @@
 import React from 'react';
 
-import Input from '@/components/Input';
-
 import './index.css';
 
 const LoginComponent = (props) => (
   <div className="login_component">
     <div className="login_card">
       <h3>
-        {
-          props.screen === 'register' ?
-            'Register' :
-            'Log in'
-        }
+        { props.title || null}
       </h3>
-      <form>
-        {
-          props.screen === 'register' ?
-            <Input
-              id="name"
-              type="ẗext"
-              placeholder="Jack-Edward Oliver"
-            /> : null
-        }
-        <Input
-          id="username"
-          type="email"
-          placeholder="mrjackolai@gmail.com"
-        />
-        <Input
-          id="password"
-          type="password"
-          placeholder="password"
-        />
-        <div className="buttons">
-          <button onClick={props.onChangeScreen}>
-            {props.screen === 'register' ? 'Log in' : 'Register'}
-          </button>
-          <button onClick={props.onSubmit}>
-            {props.screen === 'register' ? 'Register' : 'Register'}
-          </button>
-        </div>
-      </form>
+      { props.children }
     </div>
   </div>
 );
