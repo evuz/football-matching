72b2b8a8276a9cb2023bde8144be56556b7615c6
evuz <jesus_jgb_45@hotmail.com>
2017-07-12 12:26:50 +0200

    Route: remove '/' and add 'register'

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -5,8 +5,8 @@ import LoginContainer from '@/containers/Login';
 
 const RouteContainer = () => (
   <div className="route">
-    <Route path="/" exact component={LoginContainer} />
     <Route path="/login" exact component={LoginContainer} />
+    <Route path="/register" exact component={LoginContainer} />
   </div>
 );
 
