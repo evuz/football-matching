66c0a57583bb8585fa7f02542e86497910637534
evuz <jesus_jgb_45@hotmail.com>
2017-07-13 13:26:45 +0200

    loginSubmit action: setUser

--- src/app/reducers/login/actions.js
+++ src/app/reducers/login/actions.js
@@ -1,10 +1,12 @@
 import { singUp, singIn } from '@/utils/api';
+import { setUser } from '@/reducers/user';
 
 export function loginSubmit(values) {
   return (dispatch) => {
     singIn(values)
-      .then(res => {
-        console.log(res);
+      .then(({error, user}) => {
+        if(error) return error;
+        dispatch(setUser(user));
       })
   }
 }
