47f7bdce8a44b7111fd6fbdd118f62b609d02365
evuz <jesus_jgb_45@hotmail.com>
2017-07-13 14:41:56 +0200

    App: show Loading screen while app isn't initialized

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import Header from '@/containers/Header';
 import Route from '@/containers/Route';
@@ -12,17 +13,39 @@ class App extends Component {
     this.props.loginWithToken();
   }
 
+  renderApp(initApp) {
+    if (initApp) {
+      return (
+        <div className="app_component">
+          <Header />
+          <Route />
+        </div>
+      )
+    }
+    return (
+      <div className="app_component">
+        Loading ...
+      </div>
+    )
+  }
+
   render() {
+    const { app: { initApp } } = this.props;
+    const app = this.renderApp(initApp);
     return (
       <div className="App">
-        <Header />
-        <Route />
-      </div>)
+        { app }
+      </div>
+    )
   }
 };
+
+const mapStateToProps = (state) => ({
+  app: state.app
+})
 
 const mapDispatchToProps = {
   loginWithToken
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter((connect(mapStateToProps, mapDispatchToProps)(App)));
