97514adec1ee09eff77e0c78614357e4503d5273
evuz <jesus_jgb_45@hotmail.com>
2017-07-13 14:41:07 +0200

    Login reducer: initialize app

--- src/app/reducers/login/actions.js
+++ src/app/reducers/login/actions.js
@@ -1,18 +1,24 @@
 import { signUp, signIn, tokenSignIn } from '@/utils/api';
 import { getToken } from '@/utils/localStorage';
 import { setUser } from '@/reducers/user';
+import { setInitApp } from '@/reducers/app';
 
 export function loginWithToken() {
   return (dispatch) => {
     const token = getToken();
     if (token) {
       tokenSignIn(token)
-        .then(({error, user}) => {
-          if (error) return error;
+        .then(({ error, user }) => {
+          if (error) {
+            dispatch(setInitApp(true))
+            return error;
+          }
           dispatch(setUser(user));
+          dispatch(setInitApp(true))
         })
+        .catch(() => dispatch(setInitApp(true)))
     } else {
-      // TODO: Init app
+      dispatch(setInitApp(true))
     }
   }
 }
