93a6a1c2982e104eebdb997f09218803ab2ebe9e
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-07-14 23:53:50 +0200

    Register container: MatchList under form

--- src/app/containers/Register.js
+++ src/app/containers/Register.js
@@ -3,17 +3,21 @@ import { connect } from 'react-redux';
 
 import FormsDecorator from '@/components/FormsDecorator';
 import RegisterFormContainer from '@/containers/RegisterForm';
+import MatchListContainer from '@/containers/MatchList';
 import { toogleLogin } from '@/reducers/navigation';
 import { registerSubmit } from '@/reducers/login';
 import capitalizeFirstLetter from '@/utils/filters/capitalizeFirstLetter';
 
 const RegisterContainer = (props) => (
-  <FormsDecorator title={capitalizeFirstLetter(props.screen)}>
-    <RegisterFormContainer
-      onChangeScreen={props.onChangeScreen}
-      onSubmit={props.onSubmit}
-    />
-  </FormsDecorator>
+  <div className="body">
+    <FormsDecorator title={capitalizeFirstLetter(props.screen)}>
+      <RegisterFormContainer
+        onChangeScreen={props.onChangeScreen}
+        onSubmit={props.onSubmit}
+      />
+    </FormsDecorator>
+    <MatchListContainer />
+  </div>
 )
 
 const mapStateToProps = (state) => ({
