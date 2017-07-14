037f551da9bda1596c31def17850eec8c2e77f6f
evuz <jesus_jgb_45@hotmail.com>
2017-07-14 12:47:39 +0200

    Route: use Main container

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import MainContainer from '@/containers/Main';
 import PublicRoute from '@/containers/PublicRoute';
 import PrivateRoute from '@/containers/PrivateRoute';
 import LoginContainer from '@/containers/Login';
@@ -8,6 +9,7 @@ import RegisterContainer from '@/containers/Register';
 
 const RouteContainer = () => (
   <div className="route">
+    <MainContainer path="/" exact />
     <PublicRoute path="/login" exact component={LoginContainer} />
     <PublicRoute path="/register" exact component={RegisterContainer} />
     <PrivateRoute path="/private" exact component={RegisterContainer} />
