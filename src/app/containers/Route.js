069991e466e2751a058ea096f23a9586fa6744cb
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-07-15 12:29:46 +0200

    Route: use Match container

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import MainContainer from '@/containers/Main';
 import MatchListContainer from '@/containers/MatchList';
+import MatchContainer from '@/containers/Match';
 import PublicRoute from '@/containers/PublicRoute';
 import PrivateRoute from '@/containers/PrivateRoute';
 import CreatePlayerRoute from '@/containers/CreatePlayerRoute';
@@ -14,8 +15,8 @@ const RouteContainer = () => (
     <MainContainer path="/" exact />
     <PublicRoute path="/login" exact component={LoginContainer} />
     <PublicRoute path="/register" exact component={RegisterContainer} />
-    <PrivateRoute path="/private" exact component={RegisterContainer} />
     <Route path="/matchs" exact component={MatchListContainer} />
+    <PrivateRoute path="/match/:id" exact component={MatchContainer} />
     <CreatePlayerRoute path="/createOwnPlayer" exact />
   </Switch>
 );
