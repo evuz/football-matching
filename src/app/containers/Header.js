5c8294bc7fffe8ce7b44c9820465c1df0b1b6ec5
evuz <jesus_jgb_45@hotmail.com>
2017-07-16 18:59:34 +0200

    Header container: send user props to header component

--- src/app/containers/Header.js
+++ src/app/containers/Header.js
@@ -1,10 +1,30 @@
+import React from 'react';
 import { connect } from 'react-redux';
 
 import HeaderComponent from '@/components/Header';
 import { goLogin } from '@/reducers/navigation';
+import { removeUser } from '@/reducers/user';
+
+const HeaderContainer = ({ user, goLogin, removeUser }) => {
+  const headerRenderer = user.email ?
+    <HeaderComponent
+      img={user.avatar}
+      alt={user.displayName}
+      onClickImg={removeUser}
+    /> :
+    <HeaderComponent
+      onClickImg={goLogin}
+    />;
+  return headerRenderer;
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user
+})
 
 const mapDispatchToState = {
-  onClickLogin: goLogin
+  goLogin,
+  removeUser
 }
 
-export default connect(null, mapDispatchToState)(HeaderComponent);
+export default connect(mapStateToProps, mapDispatchToState)(HeaderContainer);
