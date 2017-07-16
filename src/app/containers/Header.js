ec7fd44f27eb8f710bea23a798bba8e03685c663
evuz <jesus_jgb_45@hotmail.com>
2017-07-16 19:31:49 +0200

    Header container: use goMain when click on title

--- src/app/containers/Header.js
+++ src/app/containers/Header.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import HeaderComponent from '@/components/Header';
-import { goLogin } from '@/reducers/navigation';
+import { goLogin, goMain } from '@/reducers/navigation';
 import { removeUser } from '@/reducers/user';
 
-const HeaderContainer = ({ user, goLogin, removeUser }) => {
+const HeaderContainer = ({ user, goLogin, removeUser, goMain }) => {
   const headerRenderer = user.email ?
     <HeaderComponent
       img={user.avatar}
       alt={user.displayName}
       onClickImg={removeUser}
+      onClickTitle={goMain}
     /> :
     <HeaderComponent
       onClickImg={goLogin}
@@ -24,6 +25,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToState = {
   goLogin,
+  goMain,
   removeUser
 }
 
