3b6d968b0938cae0d3fb4b6f18fdf7a0365fc6ca
evuz <jesus_jgb_45@hotmail.com>
2017-07-16 18:57:26 +0200

    Navigation reducer: add goUserOptions action

--- src/app/reducers/navigation/actions.js
+++ src/app/reducers/navigation/actions.js
@@ -3,13 +3,19 @@ import { push } from 'react-router-redux';
 
 export function goLogin(user) {
   return (dispatch) => {
-    dispatch(push('login'))
+    dispatch(push('/login'))
   }
 }
 
 export function toogleLogin() {
   return (dispatch, getState) => {
     const { pathname } = getState().route.location;
-    dispatch(push(pathname === '/login' ? 'register' : 'login'))
+    dispatch(push(pathname === '/login' ? '/register' : '/login'))
+  }
+}
+
+export function goUserOptions(user) {
+  return (dispatch) => {
+    dispatch(push('/user/options'))
   }
 }
